ed015231edbc7e9248dd52f9c68d35229c5fe09c
Aizat Faiz <aizat.faiz@gmail.com>
2011-12-17 10:51:03 +0000

    Made it even simplier

--- AizattoXHPBundle.php
+++ AizattoXHPBundle.php
@@ -3,7 +3,16 @@
 namespace Aizatto\Bundle\XHPBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use xhp_symfony__base;
+use xhp_x__base;
 
 class AizattoXHPBundle extends Bundle
 {
+
+  public function boot() {
+    xhp_symfony__base::setContainer($this->container);
+    xhp_x__base::$ENABLE_VALIDATION =
+      $this->container->get('kernel')->isDebug();
+  }
+
 }
